ab6a1e4b3b7dea3b0a0414682eb7f6a018624f14
jdalmeria-tech <alm24008@byui.edu>
2024-11-04 23:26:47 +0800

    prep1 finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        // Add a break line
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+
     }
 }
