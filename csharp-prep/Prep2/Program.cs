bcda80f0c4737e81f0521e1f3c0d5c84946f3868
jdalmeria-tech <alm24008@byui.edu>
2024-11-05 11:25:42 +0800

    core requirements achieved

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string valueFromUser = Console.ReadLine();
+
+        int grade = int.Parse(valueFromUser);
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Letter grade printed once
+        Console.WriteLine($"Grade: {letter}");
+
+        // check if the student passed
+
+        if (grade >= 70)
+        {
+            Console.Write("Congrats! You passed the course (:");
+        }
+        else
+        {
+            Console.Write("I'm sorry, try again next time ):");
+        }
     }
 }
