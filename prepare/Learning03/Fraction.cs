cbfa68cd3c20655eff92545e9432efb120f26a72
jdalmeria-tech <alm24008@byui.edu>
2024-11-19 01:00:18 +0800

    updated the class fraction and some of the main program

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,31 +1,71 @@
 public class Fraction
 {
+  // atributes...
   private int _top;
   private int _bottom;
 
-// initialize 0/1; this is just a default
-// remember denominator (bottom) MUST not be zero(0)
+  // constructors...
+  // initialize 1/1; this is just a default
+  // remember denominator (bottom) MUST not be zero(0)
   public Fraction()
   {
-    _top = 0;
-    _bottom = 1; 
+    _top = 1;
+    _bottom = 1;
   }
 
   public Fraction (int wholeNumber)
   {
-    wholeNumber = _top / _bottom;
+    _top = wholeNumber;
+    _bottom = 1;
   }
 
   public Fraction (int top, int bottom)
   {
+    if (bottom == 0)
+    {
+      throw new ArgumentException("Denominator cannot be zero.");
+    }
+
     _top = top;
+    _bottom = bottom;
+
+  }
+
+  // getters and setters...
+  public int GetTop()
+  {
+    return _top;
+  }
+
+  public void SetTop (int top)
+  {
+    _top = top;
+  }
+
+  public int GetBottom()
+  {
+    return _bottom;
+  }
+
+  public void SetBottom (int bottom)
+  {
+    if (bottom == 0)
+    {
+      throw new ArgumentException("Denominator cannot be zero.");
+    }
+
     _bottom = bottom;
   }
 
-  
+  // methods...
+  public string GetFractionString()
+  {
+    return $"{_top}/{_bottom}";
+  }
 
-
-
-
+  public double GetDecimalValue()
+  {
+    return (double)_top / _bottom;
+  }
 
 }
