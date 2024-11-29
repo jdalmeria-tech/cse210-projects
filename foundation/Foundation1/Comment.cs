3dcb1597c1c4f998350fe3fa5abfd6c52260667f
jdalmeria-tech <alm24008@byui.edu>
2024-11-29 22:30:53 +0800

    finished the classes Video and Comments. Will do the Program.cs later :)

--- foundation/Foundation1/Comment.cs
+++ foundation/Foundation1/Comment.cs
@@ -5,6 +5,13 @@ public class Comment
   public string _name;
   public string _text;
 
+  // constructor to initialize comment details
+  public Comment(string name, string text)
+  {
+    _name = name;
+    _text = text;
+  }
+
   public void DisplayInfo()
   {
     Console.WriteLine($"{_name}: {_text}");
--- foundation/Foundation1/Video.cs
+++ foundation/Foundation1/Video.cs
@@ -1,16 +1,40 @@
 using System;
+using System.Collections.Generic;
 
 public class Video
 {
   public string _title;
   public string _author;
   public int _lengthSeconds;
-
   public List<Comment> _comments = new List<Comment>();
 
+  // constructor to intialize video details
+  public Video(string title, string author, int lengthSeconds)
+  {
+    _title = title;
+    _author = author;
+    _lengthSeconds = lengthSeconds;
+  }
+
+  // method to count the number of comments
+  public int NumberOfComments()
+  {
+    return _comments.Count;
+  }
+
+  // display video details and comments
   public void DisplayInfo()
   {
     Console.WriteLine($"Video Title: {_title}");
     Console.WriteLine($"Creator: {_author}");
+    Console.WriteLine($"Length: {_lengthSeconds / 60} min {_lengthSeconds % 60} sec");
+    Console.WriteLine($"Number of Comments: {NumberOfComments()}");
+
+    Console.WriteLine("\nComments:");
+    foreach (Comment comment in _comments)
+    {
+      comment.DisplayInfo();
+    }
+    Console.WriteLine();
   }
 }
