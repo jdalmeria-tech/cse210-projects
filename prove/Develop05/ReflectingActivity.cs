718ef41f0fc8123915e34329e4785cdb481fa320
jdalmeria-tech <alm24008@byui.edu>
2024-12-10 00:47:13 +0800

    added a prompt for the reflecting activity

--- prove/Develop05/ReflectingActivity.cs
+++ prove/Develop05/ReflectingActivity.cs
@@ -9,7 +9,8 @@ public class ReflectingActivity : Activity
     "Think of a time when you stood up for someone else.",
     "Think of a time when you did something really difficult.",
     "Think of a time when you helped someone in need.",
-    "Think of a time when you did something truly selfless."
+    "Think of a time when you did something truly selfless.",
+    "Think of an experience when you were having a hard time forgiving someone."
   };
   private List<string> _questions = new List<string>
   {
