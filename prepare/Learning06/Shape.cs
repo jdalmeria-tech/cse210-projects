9f43162deaf7f9e7364561248a66c2eeeb34aee7
jdalmeria-tech <alm24008@byui.edu>
2024-12-11 23:53:55 +0800

    needs more code to write (:

---
+++ prepare/Learning06/Shape.cs
@@ -0,0 +1,22 @@
+using System;
+public abstract class Shape
+{
+  protected string _color;
+
+  public Shape()
+  {
+  }
+
+  public string GetColor()
+  {
+    return _color;
+  }
+
+  public void SetColor(string color)
+  {
+    _color = color;
+  }
+
+  public abstract double GetArea();
+
+}
