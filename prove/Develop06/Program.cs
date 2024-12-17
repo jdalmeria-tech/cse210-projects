678f689e32ca4dcf44b48dfb3692ad850b80c64f
jdalmeria-tech <alm24008@byui.edu>
2024-12-18 00:36:59 +0800

    left a comment for my work. Cheers

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -1,5 +1,8 @@
 using System;
-
+// the only stretch challenge that I can think of is to handle
+// errors smoothly
+// have been busy with other responsibilities, I did my very best to finish this project :(
+// thanks for checking it
 class Program
 {
     static void Main(string[] args)
